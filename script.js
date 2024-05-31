102dcc7d420dc6a78a6b219252747095cd97353a
smoeek <sofijamisuric@gmail.com>
2024-05-31 17:23:49 +0200

    Add a button to the html file, edit it a bit in css and make it functional in js file

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const container = document.querySelector('.container');
+const btn = document.querySelector('button');
+let divNum = 16;
+btn.addEventListener('click', () => {
+    divNum = prompt('Enter the amount of squares you want');
+    creatingDivs();
+})
 
 function creatingDivs() {
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < divNum; i++) {
     let divRow = document.createElement('div');
     divRow.setAttribute('class', 'divRow');
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < divNum; j++) {
         let divs = document.createElement('div');
         divs.setAttribute('class', 'innerDiv');
         divRow.appendChild(divs);
@@ -18,3 +24,10 @@ for (let i = 0; i < 16; i++) {
 creatingDivs();
 
 
+
+
+// let person = prompt("Please enter your name", "Harry Potter");
+//   if (person != null) {
+//     document.getElementById("demo").innerHTML =
+//     "Hello " + person + "! How are you today?";
+//   }
